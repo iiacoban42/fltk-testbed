d2b99fff8346a0b2a14a8adff4ef9cb9308e16ef
AndrewStefan <andrei.al.stefan@gmail.com>
2021-10-04 18:44:04 +0200

    fix parsing issue for trainjob logs and add feedback messages in log.sh

--- compiling_results/log.sh
+++ compiling_results/log.sh
@@ -1,4 +1,8 @@
 ./log_deployment.sh "trainjob" > trainjobs.log
+echo "got trainjob logs"
 ./log_deployment.sh "fl-server" > fl-server.log
+echo "got fl-server logs"
 python3 parse_fl_server.py
+echo "parsed fl-server logs"
 python3 parse_trainjobs.py
+echo "parsed trainjob logs"
