f4bd19666f3c41f299730038999101d4c03aa14e
AndrewStefan <andrei.al.stefan@gmail.com>
2021-10-04 19:54:22 +0200

    Added a script for merging results from the logs and generating statistics for the experiment

--- compiling_results/log.sh
+++ compiling_results/log.sh
@@ -6,3 +6,5 @@ python3 parse_fl_server.py
 echo "parsed fl-server logs"
 python3 parse_trainjobs.py
 echo "parsed trainjob logs"
+python3 merge_statistics.py
+echo "final results generated in statistics.txt"
