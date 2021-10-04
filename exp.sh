8fbee1e1db660a57cc5a32014ae4a659e6f435d3
Paula Iacoban <i.p.iacoban@gmail.com>
2021-10-04 20:35:28 +0200

    remove exp

--- exp.sh
+++
@@ -1,6 +0,0 @@
-DOCKER_BUILDKIT=1 docker build . --tag gcr.io/focal-shape-325414/fltk
-docker push gcr.io/focal-shape-325414/fltk
-cd charts
-helm uninstall -n test orchestrator
-helm install orchestrator ./orchestrator --namespace test -f fltk-values.yaml
-cd ..
