8c9f9649256f42c81fee2f7b88a217284c34df35
Paula Iacoban <i.p.iacoban@gmail.com>
2021-10-24 00:59:20 +0200

    Add script for boxplots

--- compiling_results/results/compile_charts.py
+++ compiling_results/results/compile_charts.py
@@ -1,55 +1,68 @@
-"""Create pie chart stats"""
-import matplotlib.pyplot as plt
-import csv
-import pickle
-import time
-import datetime
-import os
+"""Create boxplots"""
 import pandas as pd
-from collections import Counter
+import numpy as np
+from itertools import product
+import os
+import statsmodels.stats.multicomp
+import matplotlib.pyplot as plt
 
-zero = os.getcwd() + '/0.f.csv'
-one_c = os.getcwd() + '/1.c.csv'
-one_f = os.getcwd() + '/1.f.csv'
-two_c = os.getcwd() + '/2.c.csv'
-two_f = os.getcwd() + '/2.f.csv'
-three_c = os.getcwd() + '/3.c.csv'
-three_f = os.getcwd() + '/3.f.csv'
 
-def create_pie_chart(labels, sizes, name):
-    patches, texts = plt.pie(sizes, startangle=90)
-    plt.legend(patches, labels, loc="lower center")
-    plt.axis('equal')
-    plt.tight_layout()
-    plt.savefig(name + '.png')
+def get_boxplot(exp, data, labels):
+
+    fig = plt.figure(figsize =(10, 7))
+    # Creating axes instance
+
+    # Creating plot
+    bp = plt.boxplot(data)
+
+    # show plot
+    plt.savefig(exp+".jpeg")
     plt.show()
 
 
-def generate_categorized_blocking_websites_pie():
-    urls_file_path = "i2p/i2p/scraper/classified.csv"
-    lables = []
-    with open(urls_file_path) as csv_file:
-        reader = csv.reader(csv_file)
-        for i, line in enumerate(reader):
-            lables.append(line[1])
+def get_data(exp, exp_result, factor):
+    # pd.set_option('display.float_format',  '{:,.5f}'.format)
+    datafile = os.getcwd() + '/compiling_results/results/'+ exp +'.csv'
 
-    freq = dict(Counter(lables))
-    freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
+    df = pd.read_csv(datafile)
 
-    values = []
-    lables = []
-    n = sum(freq.values())
-    for key, val in freq.items():
-        p = round(val / n * 100, 2)
-        lables.append(key + " " + str(p) + "%")
-        values.append(p)
-    create_pie_chart(lables, values,  "categories")
+    factor_config = df[factor].unique()
 
-def generate_pie_charts():
-    generate_blocking_proportions()
-    generate_categorized_blocking_websites_pie()
+    data = []
+    boxplot_df = pd.DataFrame([])
+    boxplot_df.rename(columns = factor_config)
 
-generate_pie_charts()
-#z_stat: 6.394, p_value: 0.000
-#Reject the null hypothesis - suggest the alternative hypothesis is true
-test_hypothesis_proportions()
+    for fact in factor_config:
+        # filter rows by factor config
+        new_df = df[df[factor]==fact]
+        # get accuracy/response time
+        new_data = new_df[exp_result]
+        data.append(new_data)
+        print(fact)
+
+        boxplot_df[fact] = new_data
+
+    boxplot_df[factor_config[0]] = data[0]
+    boxplot_df[factor_config[1]] = data[1]
+    print(boxplot_df)
+
+    myFig = plt.figure()
+    bp = boxplot_df.boxplot()
+    myFig.savefig(exp+".jpg", format="jpg")
+
+    # get_boxplot(exp, data, factor_config)
+
+
+exp = '0.f'
+exp_result = {
+        'A': "accuracy",
+        'T': "response_time"
+    }
+factors = {
+        'C': "cores",
+        'M': "memory",
+        'B': "batch_size",
+        'E': "epochs"
+    }
+
+get_data(exp, exp_result['T'], factors['E'])
